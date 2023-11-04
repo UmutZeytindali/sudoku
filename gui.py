9da4c323e083ad66547e93bcf53cb2f5541891e5
Umut Zeytindalı <umutzeytindali@outlook.com>
2023-11-04 21:05:56 +0300

    update

--- gui.py
+++ gui.py
@@ -1,10 +1,20 @@
 import pygame as pg
+from sys import exit
 
 pg.init()
-screen = pg.display.set_mode((800, 400))
 
-while 1:
+# misc. vars
+screen = pg.display.set_mode((600, 700))
+pg.display.set_caption("Sudoku Çözücü")
+WINDOW_ICON = pg.image.load("sudoku/sudoku.svg")
+pg.display.set_icon(WINDOW_ICON)
+clock = pg.time.Clock()
+
+while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
+            exit()
+
     pg.display.update()
+    clock.tick(60)
