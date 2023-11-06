a67d7ad0ee4a531656cdffb8e6f737aa73852bd3
Umut Zeytindalı <umutzeytindali@outlook.com>
2023-11-06 03:15:44 +0300

    updt

--- gui.py
+++ gui.py
@@ -1,16 +1,70 @@
 import pygame as pg
-from sys import exit
+import numpy as np
+import sys
 
 pg.init()
 
+grid = [
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
 # misc. vars
-screen = pg.display.set_mode((600, 700))
-pg.display.set_caption("Sudoku Çözücü")
+screen = pg.display.set_mode((750, 750))
+pg.display.set_caption("Sudoku")
 WINDOW_ICON = pg.image.load("sudoku/sudoku.svg")
 pg.display.set_icon(WINDOW_ICON)
 clock = pg.time.Clock()
+font = pg.font.SysFont(None, 80)
 
-while True:
+
+def draw_background():
+    screen.fill(pg.Color("gray"))
+    pg.draw.rect(screen, pg.Color("black"), pg.Rect(15, 15, 720, 720), 10)
+    i = 1
+    while (i * 80) < 720:
+        line_width = 5 if i % 3 > 0 else 10
+        pg.draw.line(
+            screen,
+            pg.Color("Black"),
+            pg.Vector2((i * 80) + 15, 15),
+            pg.Vector2((i * 80) + 15, 735),
+            line_width,
+        )
+        pg.draw.line(
+            screen,
+            pg.Color("Black"),
+            pg.Vector2(15, (i * 80) + 15),
+            pg.Vector2(735, (i * 80) + 15),
+            line_width,
+        )
+        i += 1
+
+
+def draw_numbers():
+    global grid
+    row = 0
+    offset = 40
+    while row < 9:
+        col = 0
+        while col < 9:
+            output = grid[row][col]
+            n_text = font.render(str(output), True, pg.Color("Black"))
+            screen.blit(
+                n_text, pg.Vector2((col * 80) + offset + 5, (row * 80) + offset - 2)
+            )
+            col += 1
+        row += 1
+
+
+def game_loop():
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
@@ -18,3 +72,9 @@ while True:
 
     pg.display.update()
     clock.tick(60)
+    draw_background()
+    draw_numbers()
+
+
+while True:
+    game_loop()
--- sudoku.py
+++ sudoku.py
@@ -1,12 +1,7 @@
 import numpy as np
 import sys
 
-
-def main():
-    solution()
-
-
-sudoku_grid = [
+grid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -20,38 +15,36 @@ sudoku_grid = [
 
 
 def check_num(y, x, n):
-    global sudoku_grid
+    global grid
     for i in range(0, 9):
-        if sudoku_grid[y][i] == n:
+        if grid[y][i] == n:
             return False
     for i in range(0, 9):
-        if sudoku_grid[i][x] == n:
+        if grid[i][x] == n:
             return False
 
     x0 = (x // 3) * 3
     y0 = (y // 3) * 3
     for i in range(0, 3):
         for j in range(0, 3):
-            if sudoku_grid[y0 + i][x0 + j] == n:
+            if grid[y0 + i][x0 + j] == n:
                 return False
     return True
 
 
 def solution():
-    global sudoku_grid
+    global grid
     for y in range(0, 9):
         for x in range(0, 9):
-            if sudoku_grid[y][x] == 0:
+            if grid[y][x] == 0:
                 for n in range(1, 10):
                     if check_num(y, x, n):
-                        sudoku_grid[y][x] = n
+                        grid[y][x] = n
                         solution()
-                        sudoku_grid[y][x] = 0
-                return
-    print(np.matrix(sudoku_grid), end="")
+                        grid[y][x] = 0
+                return grid
+    print(np.matrix(grid))
     sys.exit("")
 
 
-
-if __name__ == "__main__":
-    main()
+solution()
